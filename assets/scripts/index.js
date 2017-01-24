ab0c074a182248dbeaea9f15f18916c80bc90376
yourUsername <your_email@example.com>
2017-01-24 11:36:52 -0500

    my work

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,5 +7,8 @@ $(() => {
   setAPIOrigin(location, config);
 });
 
+$('.dropdown').on('show.bs.dropdown', function () {
+  console.log("works");
+});
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example');
